f592890cfeb38d6ae4107fd55c492a0d01b22e10
Sachin Pasi <sachin.pasi-v@adityabirlacapital.com>
2024-06-04 17:47:29 +0530

    feat:(responsive)

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="mx-auto flex w-[600px] flex-col gap-y-14 pt-12">
+      <div className="mx-auto flex w-[90%] flex-col gap-y-14 pt-7 lg:w-[600px] lg:pt-12">
         <div className="flex items-center">
           <div className="flex items-center gap-[10px]">
             <Avatar />
@@ -25,7 +25,7 @@ const Home = () => {
             </div>
           </div>
           <div className="flex flex-shrink-0 flex-grow basis-0 items-center justify-end gap-[10px]">
-            <div className="flow-row flex items-center gap-[10px]">
+            <div className="flow-row hidden items-center gap-[10px] lg:flex">
               <div className="h-[7px] w-[7px] rounded-full bg-[#09b858]"></div>
               <p className="text-[12px] text-[#FFFFFFCC]">Open to work</p>
             </div>
@@ -43,21 +43,27 @@ const Home = () => {
 
         <div className="float-start flex flex-col flex-nowrap items-start justify-center gap-[25px]">
           <div className="flex flex-col items-start gap-1">
-            <div className="text-[28px] leading-[1.3em]">{meta.tagline}</div>
-            <div className="text-[28px] leading-[1.3em] text-[#8A8A8A]">
+            <div className="text-[20px] leading-[1.3em] lg:text-[28px]">
+              {meta.tagline}
+            </div>
+            <div className="text-[20px] leading-[1.3em] text-[#8A8A8A] lg:text-[28px]">
               {meta.subTagline}
             </div>
           </div>
           <div className="flex flex-col gap-[25px]">
-            <p className="text-[15px] leading-[27px] text-[#FFFFFFCC]">
+            <p className="text-[14px] leading-[1.8em] text-[#FFFFFFCC] lg:text-[15px] lg:leading-[27px]">
               {meta.description}
             </p>
-            <div className="flex items-center gap-[7px] text-[15px] font-light text-[#FFFFFFCC]">
+            <div className="hidden items-center gap-[7px] text-[15px] font-light text-[#FFFFFFCC] lg:flex">
               <p>Press</p>
               <div className="cursor-pointer rounded-[5px] border-[1px] border-[#3B3B3B] px-2">
                 <p className="text-[13px]">S</p>
               </div>
               <p>anytime to send me an email</p>
+            </div>
+            <div className="flow-row flex max-w-[130px] items-center gap-[10px] rounded-lg border-[1px] border-[#262626] p-3 lg:hidden">
+              <div className="h-[7px] w-[7px] rounded-full bg-[#09b858]"></div>
+              <p className="text-[12px] text-[#FFFFFFCC]">Open to work</p>
             </div>
           </div>
         </div>
@@ -74,7 +80,7 @@ const Home = () => {
               Some of the tools I use in my workflow.
             </p>
           </div>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 gap-2 lg:grid-cols-3 lg:gap-4">
             {meta.techStack.map(
               ({ Icon, name, subHeading, bg = "bg-white" }, idx) => (
                 <div
@@ -94,7 +100,7 @@ const Home = () => {
                   </div>
                   <div>
                     <h4 className="text-[14px] leading-[1.3em]">{name}</h4>
-                    <p className="text-[11.5px] font-light leading-[1.6em] text-[#FFFFFFCC]">
+                    <p className="text-[10px] font-light leading-[1.6em] text-[#FFFFFFCC] lg:text-[11.5px]">
                       {subHeading}
                     </p>
                   </div>
@@ -105,6 +111,32 @@ const Home = () => {
         </div>
 
         <div className="h-[1px] w-full bg-[#262626]"></div>
+
+        <div className="mb-8">
+          <p className="mt-8 text-[12px] font-light text-white/90">
+            © 2024 — Built with{"  "}
+            <a
+              href="https://nextjs.org"
+              className="hover:text-white hover:underline hover:decoration-rose-300/30 hover:underline-offset-2 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-rose-500/70"
+            >
+              Next.js
+            </a>
+            ,
+            <a
+              href="https://tailwindcss.com"
+              className="hover:text-white hover:underline hover:decoration-rose-300/30 hover:underline-offset-2 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-rose-500/70"
+            >
+              Tailwind{"  "}
+            </a>
+            and{"  "}
+            <a
+              href="https://vercel.com"
+              className="hover:text-white hover:underline hover:decoration-rose-300/30 hover:underline-offset-2 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-rose-500/70"
+            >
+              Vercel{"  "}
+            </a>
+          </p>
+        </div>
       </div>
     </>
   );
--- ui/Avatar.tsx
+++ ui/Avatar.tsx
@@ -1,45 +1,9 @@
 const Avatar = () => {
   return (
-    <svg
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="40" height="40" rx="220" fill="url(#paint0_linear_12_14)" />
-      <rect
-        x="112"
-        y="64"
-        width="216"
-        height="312"
-        fill="url(#pattern0_12_14)"
-      />
-      <defs>
-        <pattern
-          id="pattern0_12_14"
-          patternContentUnits="objectBoundingBox"
-          width="1"
-          height="1"
-        >
-          <use
-            xlinkHref="#image0_12_14"
-            transform="scale(0.00462963 0.00320513)"
-          />
-        </pattern>
-        <linearGradient
-          id="paint0_linear_12_14"
-          x1="0"
-          y1="0"
-          x2="0"
-          y2="40"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#FCEBDE" />
-          <stop offset="1" stopColor="#F7D3BA" />
-        </linearGradient>
-      </defs>
-    </svg>
+    <div
+      style={{ width: "45px", height: "45px" }}
+      className="flex w-[45px] rounded-full bg-white"
+    ></div>
   );
 };
 
