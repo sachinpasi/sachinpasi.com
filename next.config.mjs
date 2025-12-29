9dbc0bbf54b5b9a8ff8f775ec5d9ccdbdf396501
Sachin Pasi <sachin.pasi1@adityabirlacapital.com>
2025-12-29 22:01:23 +0530

    fix : favicon and last updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+import { execSync } from 'child_process';
+
+const getLastCommitDate = () => {
+  try {
+    const timestamp = execSync('git log -1 --format=%ct').toString().trim();
+    return timestamp;
+  } catch (e) {
+    return 'Jan 1, 2025';
+  }
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_DEPLOY_TIMESTAMP: process.env.VERCEL_GIT_COMMIT_TIMESTAMP,
+    NEXT_PUBLIC_DEPLOY_TIMESTAMP:
+      process.env.VERCEL_GIT_COMMIT_TIMESTAMP || getLastCommitDate(),
   },
 };
 
